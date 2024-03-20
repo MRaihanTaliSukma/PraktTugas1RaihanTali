2a3e84a18524c798711be1059d571660042ebc0f
MRaihanTaliSukma <161023360+MRaihanTaliSukma@users.noreply.github.com>
2024-03-20 11:13:19 -0800

    Add files via upload

---
+++ Mahasiswa1.java
@@ -0,0 +1,55 @@
+public class Mahasiswa1 {
+    private String nama;
+    private String nim;
+    private String jurusan;
+    private static String universitas = "UMM";
+
+    public Mahasiswa1(String nama, String nim, String jurusan) {
+        this.nama = nama;
+        this.nim = nim;
+        this.jurusan = jurusan;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        // Contoh validasi NIM, bisa disesuaikan dengan kebutuhan
+        if (nim.matches("[0-9]+") && nim.length() == 15) {
+            this.nim = nim;
+        } else {
+            System.out.println("NIM tidak valid");
+        }
+    }
+
+    public String getJurusan() {
+        return jurusan;
+    }
+
+    public void setJurusan(String jurusan) {
+        this.jurusan = jurusan;
+    }
+
+    // Method tampil data
+    public void tampilDataMahasiswa() {
+        System.out.println("Data Mahasiswa :");
+        tampilUniversitas();
+        System.out.println("Nama: " + this.nama);
+        System.out.println("NIM: " + this.nim);
+        System.out.println("Jurusan: " + this.jurusan);
+    }
+
+    // Tampil univ pakai method static
+    public static void tampilUniversitas() {
+        System.out.println(universitas);
+    }
+}
---
+++ main1.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class main1 {
+    public static void menu() {
+        ArrayList<Mahasiswa> listMahasiswa = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Menu :\n1. Tambah Data Mahasiswa");
+            System.out.println("2. Tampilkan Data Mahasiswa");
+            System.out.println("3. Keluar");
+            System.out.print("Pilihan anda: ");
+            int Pilihan = scanner.nextInt();
+            scanner.nextLine();
+            if (Pilihan == 1) {
+                System.out.print("Masukkan Nama Mahasiswa: ");
+                String nama = scanner.nextLine();
+
+                String nim;
+                do {
+                    System.out.print("Masukkan NIM Mahasiswa : ");
+                    nim = scanner.nextLine();
+                    if (nim.length() != 15) {
+                        System.out.println("NIM harus memiliki panjang 15 digit!");
+                    }
+                } while (nim.length() != 15);
+
+                System.out.print("Masukkan jurusan mahasiswa: ");
+                String jurusan = scanner.nextLine();
+
+                Mahasiswa mahasiswa = new Mahasiswa(nama, nim, jurusan);
+                listMahasiswa.add(mahasiswa);
+
+                System.out.println("Data Berhasil Ditambahkan!!");
+            } else if (Pilihan == 2) {
+                for (Mahasiswa mahasiswa : listMahasiswa) {
+                    mahasiswa.tampilDataMahasiswa();
+                }
+            } else if (Pilihan == 3) {
+                System.out.println("Babay");
+                break;
+            } else {
+                System.out.println("Pilihan Apa ini!!");
+            }
+        }
+        scanner.close(); // Close the scanner to prevent resource leak
+    }
+
+    public static void main(String[] args) {
+        menu();
+    }
+}
